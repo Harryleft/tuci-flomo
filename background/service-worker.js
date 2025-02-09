51ee6a7fe3ed6690124c8ad183d98d8ddbdeccbe
Dongchao Shen <afkxwz@gmail.com>
2025-02-09 14:55:20 +0800

    refactor: 移除右键菜单功能

--- background/service-worker.js
+++ background/service-worker.js
@@ -1,11 +1,7 @@
 // 初始化
 chrome.runtime.onInstalled.addListener(() => {
-  // 右键菜单配置
-  chrome.contextMenus.create({
-    id: 'captureWord',
-    title: '记录单词 "%s"',
-    contexts: ['selection']
-  });
+  // 移除所有已存在的右键菜单项
+  chrome.contextMenus.removeAll();
 
   // 设置侧边栏
   if (chrome.sidePanel) {
@@ -19,31 +15,6 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error));
 });
-
-// 处理右键菜单点击
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'captureWord') {
-    handleWordCapture(info.selectionText);
-  }
-});
-
-// 处理来自 content script 的消息
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'CAPTURE_WORD') {
-    handleWordCapture(request.word);
-    sendResponse({ success: true });
-  }
-});
-
-// 处理单词捕获
-async function handleWordCapture(word) {
-  try {
-    // TODO: 实现单词处理逻辑
-    console.log('捕获单词:', word);
-  } catch (error) {
-    console.error('处理单词时出错:', error);
-  }
-}
 
 // 处理扩展图标点击
 chrome.action.onClicked.addListener((tab) => {
